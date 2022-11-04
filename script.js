dfda994f98e463d7b49eaf1a026b71df944a85b7
hassan-980 <hassansuhail760@gmail.com>
2022-11-04 19:28:24 +0530

    upadate

---
+++ script.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("nav-btn").style.display = "none";
+    document.getElementById("nav-cls").style.display = "block";
+}
+
+
+function closeNav() {
+    document.getElementById("nav-btn").style.display = "block";
+    document.getElementById("nav-cls").style.display = "none";
+}
+
