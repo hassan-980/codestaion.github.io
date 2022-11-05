2f687affdc3759d48c94e0bfe52a4e75300a6182
hassan-980 <hassansuhail760@gmail.com>
2022-11-06 02:06:23 +0530

    update

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 function openNav() {
+    document.getElementById("nav").style.height="275px";
     document.getElementById("nav-btn").style.display = "none";
     document.getElementById("nav-cls").style.display = "block";
+    document.getElementById("btn-nav").style.display = "block";
 }
 
 
 function closeNav() {
+    document.getElementById("nav").style.height="50px";
     document.getElementById("nav-btn").style.display = "block";
     document.getElementById("nav-cls").style.display = "none";
+    document.getElementById("btn-nav").style.display = "none";
 }
 
