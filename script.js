e6de1677784aa9e20d88e78f8e01fd7ee8b48a0b
hassan-980 <hassansuhail760@gmail.com>
2022-11-07 01:52:36 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function openNav() {
-    document.getElementById("nav").style.height="275px";
+    document.getElementById("nav").style.height = "275px";
     document.getElementById("nav-btn").style.display = "none";
     document.getElementById("nav-cls").style.display = "block";
     document.getElementById("btn-nav").style.display = "block";
@@ -7,9 +7,41 @@ function openNav() {
 
 
 function closeNav() {
-    document.getElementById("nav").style.height="50px";
+    document.getElementById("nav").style.height = "50px";
     document.getElementById("nav-btn").style.display = "block";
     document.getElementById("nav-cls").style.display = "none";
     document.getElementById("btn-nav").style.display = "none";
 }
+
+function closenavbar(){
+    document.getElementById("nav").style.height = "50px";
+    document.getElementById("btn-nav").style.display = "none";
+    document.getElementById("nav-btn").style.display = "block";
+    document.getElementById("nav-cls").style.display = "none";
+
+}
+
+//Javacript for the scroll indicator bar
+window.addEventListener("scroll", () => {
+    const indicatorBar = document.querySelector(".scroll-indicator-bar");
+
+    const pageScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    const scrollValue = (pageScroll / height) * 100;
+
+    indicatorBar.style.width = scrollValue + "%";
+});
+
+//Responsive navigation menu toggle
+const menuBtn = document.querySelector(".nav-menu-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const navigation = document.querySelector(".navigation");
+
+menuBtn.addEventListener("click", () => {
+    navigation.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    navigation.classList.remove("active");
+});
 
